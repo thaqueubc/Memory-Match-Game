9b065534c6bc463aa874234f9b07ac6b40028230
TonyKoo <46759104+TonyKoo@users.noreply.github.com>
2019-10-24 22:43:57 -0700

    Revise Counter, Add Reset Game, Game Over Alert

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,18 +1,12 @@
 const cards = document.querySelectorAll('.memory-card');
-totalClick = 0;
+var totalClicks = 0;
+var timeStart;
 
 let hasFlippedCard = false;
 let lockBoard = false;
 let firstCard, secondCard;
 
 function flipCard() {
-
-
-  //Update Click in HTML
-  var countClick = 1;
-  totalClick= totalClick+countClick;
-  document.getElementById("click-counter").innerHTML=totalClick;
-
   if (lockBoard) return;
   if (this === firstCard) return;
 
@@ -21,18 +15,40 @@ function flipCard() {
   if (!hasFlippedCard) {
     hasFlippedCard = true;
     firstCard = this;
+    addClickCount();
 
     return;
   }
 
   secondCard = this;
+  addClickCount();
   checkForMatch();
 }
 
-function clearCounter(){
-  document.getElementById("click-counter").innerHTML=0;
+function checkWin() {
+  var totalMatched = 0;
+  
+  //Check How Many Matched Cards
+  cards.forEach(card => {
+    if (card.style.visibility == "hidden"){
+      totalMatched += 1;
+    }
+  })
 
+  if (totalMatched == cards.length){
+    var timeTaken = new Date();
+    var timeDifference = timeTaken - timeStart;
+
+    alert("CONGRATULATIONS!\n" +
+          "You took: " + msToTime(timeDifference) + " to finish the game\n\n" +
+          "Want To Play Again? Press The 'Restart Game' Button Play Again");
+  }
 }
+
+function displayCounter(){
+  document.getElementById("click-counter").innerHTML= "# of Clicks: " + totalClicks;
+}
+
 function checkForMatch() {
   let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
 
@@ -40,8 +56,15 @@ function checkForMatch() {
 }
 
 function disableCards() {
+
+  //Unflip the cards so when board reset it's ready to go again
+  firstCard.classList.remove('flip');
+  secondCard.classList.remove('flip');
+  
   firstCard.removeEventListener('click', flipCard);
   secondCard.removeEventListener('click', flipCard);
+  
+  //Hide correct cards from view
   firstCard.style.visibility="hidden";
   secondCard.style.visibility="hidden";
   resetBoard();
@@ -61,13 +84,44 @@ function unflipCards() {
 function resetBoard() {
   [hasFlippedCard, lockBoard] = [false, false];
   [firstCard, secondCard] = [null, null];
+  checkWin();
 }
 
-(function shuffle() {
+function shuffleCards(){
   cards.forEach(card => {
-    let randomPos = Math.floor(Math.random() * 12);
+    let randomPos = Math.floor(Math.random() * cards.length);
     card.style.order = randomPos;
+    card.style.visibility = "visible";
   });
-})();
 
-cards.forEach(card => card.addEventListener('click', flipCard));
+  cards.forEach(card => card.addEventListener('click', flipCard));
+  resetCounter();
+  timeStart = new Date();
+}
+
+function addClickCount() {
+  totalClicks += 1;
+  displayCounter();
+}
+
+function resetCounter(){
+  totalClicks = 0;
+  displayCounter();
+}
+
+function msToTime(duration) {
+  // var milliseconds = parseInt((duration%1000)/100);
+   var seconds = parseInt((duration/1000)%60);
+   var minutes = parseInt((duration/(1000*60))%60);
+   var hours = parseInt((duration/(1000*60*60))%24);
+ 
+   hours = (hours < 10) ? "0" + hours : hours;
+   minutes = (minutes < 10) ? "0" + minutes : minutes;
+   seconds = (seconds < 10) ? "0" + seconds : seconds;
+ 
+   return hours + ":" + minutes + ":" + seconds;
+ }
+ 
+(function startGame() {
+  shuffleCards();
+})();
