f66b8efb2c71768ba6c4190b1b460fc7b51d4b93
TonyKoo <46759104+TonyKoo@users.noreply.github.com>
2019-10-26 18:11:50 -0700

    Combine Levels/Bug Fixes/Dynamic Card Injection/Removal

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,12 +1,14 @@
-const cards = document.querySelectorAll('.memory-card');
+var cards = document.querySelectorAll('.memory-card');
+const hardCards = ['java', 'python', 'react', 'bootstrap', 'aws', 'angular'];
 var totalClicks = 0;
 var timeStart;
-
+var hardMode = false;
 
 let hasFlippedCard = false;
 let lockBoard = false;
 let firstCard, secondCard;
 
+// <== Main Game Methods ==>
 function flipCard() {
   if (lockBoard) return;
   if (this === firstCard) return;
@@ -17,37 +19,12 @@ function flipCard() {
     hasFlippedCard = true;
     firstCard = this;
     addClickCount();
-
     return;
   }
 
   secondCard = this;
   addClickCount();
   checkForMatch();
-}
-
-function checkWin() {
-  var totalMatched = 0;
-  
-  //Check How Many Matched Cards
-  cards.forEach(card => {
-    if (card.style.visibility == "hidden"){
-      totalMatched += 1;
-    }
-  })
-
-  if (totalMatched == cards.length){
-    var timeTaken = new Date();
-    var timeDifference = timeTaken - timeStart;
-
-    alert("CONGRATULATIONS!\n" +
-          "You took: " + msToTime(timeDifference) + " to finish the game\n\n" +
-          "Want To Play Again? Press The 'Restart Game' Button Play Again");
-  }
-}
-
-function displayCounter(){
-  document.getElementById("click-counter").innerHTML= "# of Clicks: " + totalClicks;
 }
 
 function checkForMatch() {
@@ -57,7 +34,6 @@ function checkForMatch() {
 }
 
 function disableCards() {
-
   //Unflip the cards so when board reset it's ready to go again
   firstCard.classList.remove('flip');
   secondCard.classList.remove('flip');
@@ -71,15 +47,20 @@ function disableCards() {
   resetBoard();
 }
 
-function unflipCards() {
+function unflipCards(timeout = 1500) {
   lockBoard = true;
 
   setTimeout(() => {
-    firstCard.classList.remove('flip');
-    secondCard.classList.remove('flip');
-
-    resetBoard();
-  }, 1500);
+    try {
+      firstCard.classList.remove('flip');
+      secondCard.classList.remove('flip');
+    } catch (e) {
+      console.error(e);
+      console.log("NOTE: Possible Cause is Game Reset Already In Progress - Flip Class Removal Error");
+    } finally {
+      resetBoard();
+    }
+  }, timeout);
 }
 
 function resetBoard() {
@@ -89,15 +70,144 @@ function resetBoard() {
 }
 
 function shuffleCards(){
+  //Refresh card array because level may have changed
+  cards = document.querySelectorAll('.memory-card');
+
+  //Make sure all cards are set to face down
+  if (firstCard != null || secondCard != null) {
+    unflipCards(0);
+  }
+
   cards.forEach(card => {
     let randomPos = Math.floor(Math.random() * cards.length);
     card.style.order = randomPos;
     card.style.visibility = "visible";
+    card.addEventListener('click', flipCard);
   });
 
-  cards.forEach(card => card.addEventListener('click', flipCard));
   resetCounter();
-  timeStart = new Date();
+  timeStart = new Date(); //Current time to compare with finish time
+}
+// <== END Main Game Methods ==>
+
+// <== Difficulty Change Methods ==>
+function changeDifficulty() {
+  let difficultyButton = document.getElementById('change-difficulty');
+  let modeText = "Difficulty: ";
+
+  if (hardMode) { //Hard => Normal
+    difficultyButton.innerHTML = modeText + "Normal";
+    difficultyButton.classList.remove('hardButton');
+    hardMode = false;
+    modifyGameBoardWidth('normal');
+    addRemoveHardCards('remove');
+    adjustCardWidth("calc(25% - 10px)");
+    shuffleCards();
+  } else if (!hardMode) { //Normal => Hard
+    difficultyButton.innerHTML = modeText + "Hard";
+    difficultyButton.classList.add('hardButton');
+    hardMode = true;
+    modifyGameBoardWidth('hard');
+    addRemoveHardCards('add');
+    adjustCardWidth('calc(12% - 10px)');
+    shuffleCards();
+  }
+} 
+
+function addMemoryCard(cardName) {
+  var gameBoardDiv = document.getElementById('gameBoard');
+
+  //Inject 2 New Divs for Matching Cards
+  for (let i = 0; i < 2; i++) {
+    var div = document.createElement('div');
+    div.className = 'memory-card';
+    div.dataset.framework = cardName;
+    gameBoardDiv.appendChild(div);
+    }
+
+  //Grab both new Divs
+  var newMemoryCard = document.querySelectorAll('[data-framework=' + cardName + ']');
+
+  for (let i = 0; i < newMemoryCard.length; i++) {
+    //Create images for card front and back
+    var newCardFront = document.createElement('img');
+    newCardFront.className = 'front-face';
+    newCardFront.src = 'images/' + cardName +'.png';
+    newCardFront.alt = cardName;
+
+    var newCardBack = document.createElement('img');
+    newCardBack.className = 'back-face';
+    newCardBack.src = 'images/SSD.png';
+    newCardBack.alt = 'SSD';
+    
+    //Inject Images into each Div
+    newMemoryCard[i].appendChild(newCardFront);
+    newMemoryCard[i].appendChild(newCardBack);
+  }
+}
+
+function addRemoveHardCards(operation){
+  if(operation == 'add') {
+    hardCards.forEach(hardCard => {
+      addMemoryCard(hardCard);
+    });
+  } else if (operation == 'remove') {
+    hardCards.forEach(hardCard => {
+      var cardToRemove = document.querySelectorAll('[data-framework=' + hardCard + ']');
+      for (let i = 0; i < cardToRemove.length; i++){
+        cardToRemove[i].remove();
+      }
+    });
+  }
+}
+// <== END Difficulty Change Methods ==>
+
+// <== Document Styling Methods ==>
+function modifyGameBoardWidth(difficulty) {
+  let gameBoard = document.getElementById('gameBoard').style;  
+  if (difficulty == 'hard') {
+    gameBoard.width = "1200px";
+  } else if (difficulty == 'normal') {
+    gameBoard.width = "640px";
+  }
+}
+
+function adjustCardWidth(newWidth){
+  let cardToAdjust = document.getElementsByClassName('memory-card');
+  for (i = 0; i < cardToAdjust.length; i++) {
+    cardToAdjust[i].style.width = newWidth;
+  }
+}
+// <== End Document Styling Methods ==>
+
+// <=== Display Methods ==>
+function checkWin() {
+
+  //Check How Many Matched Cards 
+  //Bypass if Total Clicks < # of Cards because
+  //No possible way they have won
+  if (totalClicks >= cards.length) {
+    var totalMatched = 0;
+
+    cards.forEach(card => {
+      if (card.style.visibility == "hidden"){
+        totalMatched += 1;
+      }
+    })
+
+    if (totalMatched == cards.length){
+      var timeTaken = new Date();
+      var timeDifference = timeTaken - timeStart;
+
+      alert("CONGRATULATIONS!\n" +
+            "You needed " + totalClicks + " clicks and took " + msToTime(timeDifference) + " to finish the game\n\n" +
+            "Want To Play Again? Press The 'Restart Game' Button Play Again");
+    }
+  }
+}
+
+function displayCounter(){
+  document.getElementById("click-counter").innerHTML= "# of Clicks: " + totalClicks;
 }
 
 function addClickCount() {
@@ -122,7 +232,9 @@ function msToTime(duration) {
  
    return hours + ":" + minutes + ":" + seconds;
  }
- 
+ // <=== END Display Methods ==>
+
+//Auto-Launch Game
 (function startGame() {
   shuffleCards();
 })();
